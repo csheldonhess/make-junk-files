3c31cadfda5b12122725b1ffce9b44c1f18e2775
Coral <coral@sheldon-hess.org>
2016-02-16 19:06:20 -0500

    And now files are in there, boom!

--- junkfiles.py
+++ junkfiles.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-""" make a specified number of junk files 
-    in a directory of your choosing
+""" Make a specified number of junk files in a directory of your choosing.
+    There is no default directory--that has to be specified.
+    The default number of files is 10. If you don't specify, you get 10 files.
 
 usage: ./junkfiles.py directory number_of_files
-	   ./junkfiles.py junkfiledir 100
+       ./junkfiles.py junkfiledir 100
 
 """
 
@@ -23,13 +24,20 @@ directory = sys.argv[1]
 
 if os.path.exists(directory):
     print "Directory exists"
-else: 
-	os.mkdir(directory)
-	print "Now directory exists!"
-
+else:
+    try:  
+        os.mkdir(directory)
+        print "Now directory exists!"
+    except:
+        sys.exit("Invalid directory structure, not cool. Exiting.")
+        
 try:
-	number = sys.argv[2]
+    number = sys.argv[2]
 except IndexError:
-	number = 10
+    number = 10
 
-#os.system("touch filename.extension")
+filename = "roboto.txt"
+
+fully_specified_file = os.path.join(directory, filename)
+
+os.system("touch " + fully_specified_file)
